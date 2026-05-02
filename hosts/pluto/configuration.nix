f7e3bb1db9e6f0dbe3d5240f5dc8b9bc5b3ff99f
pluto <pluto@nebula>
2026-05-02 03:04:55 +0000

    pluto: added syncthing

--- hosts/pluto/configuration.nix
+++ hosts/pluto/configuration.nix
@@ -10,7 +10,25 @@
     tree
     bitwarden-desktop
     pkgs.qbittorrent
-    syncthing
   ];
+
+  services.syncthing = {
+    enable = true;
+
+    # Run it as your normal user (recommended for a desktop/dev machine)
+    user = "voyager";
+    group = "users";
+
+    # Where to store config + data (change if you want)
+    configDir = "/home/voyager/.config/syncthing";
+    dataDir = "/home/voyager/Sync"; # or wherever you want your synced folders
+
+    # Allow you to change devices/folders from the web GUI
+    overrideDevices = true;
+    overrideFolders = true;
+
+    # (Optional but nice) open the GUI only on localhost
+    guiAddress = "127.0.0.1:8384";
+  };
 
 }
