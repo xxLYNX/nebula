ecbc8d3beda1087553b81e44bb54197580995855
pluto <pluto@nebula>
2026-05-03 20:13:34 +0000

    desktop: added io.nix module for automount with udiskie

---
+++ modules/desktop/home/io.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.homeManager.desktop or { };
+in
+lib.mkIf (cfg.enable or false) {
+  services.udiskie = {
+    enable = true;
+    automount = true;
+    notify = true;
+    tray = true;
+    settings = { };
+  };
+}
