9b330ee8e267992b01873b98d6daa711b4ea94db
pluto <pluto@nebula>
2026-07-30 04:13:12 +0000

    added latex but properly this time I hope

--- hosts/pluto/configuration.nix
+++ hosts/pluto/configuration.nix
@@ -20,7 +20,7 @@
     gnumake
     android-tools # gives adb, fastboot
     android-studio
-    texlive.combined.scheme-medium # latex-for-resume-repo
+    texlive.combined.scheme-full # latex-for-resume-repo
   ];
 
   services.power-profiles-daemon.enable = true;
